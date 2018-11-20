e7feb25c5ff83d438797b87edb5ebcf0fcebf166
Kirill Skubnikov <kirilllik@gmail.com>
2018-11-21 00:47:45 +0300

    PDF parser now outputs in CSV XML parser init commit

--- pdf/main.py
+++ pdf/main.py
@@ -1,20 +1,25 @@
 import PyPDF2
 import re
+import csv
 
 if __name__ == '__main__':
     regex = ur"(\d{2}\s\w{3})\s{2}-\s{2}(\d{2}\s\w{3})([1-7 ]*)(\d{2}:\d{2})(\d{2}:\d{2}(\+1)?)(\w{2}\d*\*?)(.{3})(\d{1,2}H\d{1,2}M)"
     start_page_num = 4
     end_page_num = 27514
-    cell_delimiter = '\t'
-    row_delimiter = '\n'
+    cell_delimiter = ';'
     nested_regex_group_numbers = {6}
+    header = ["validity_from", "validity_to", "days", "dep_time", "arr_time", "flight", "aircraft", "travel_time"]
 
-    input_file = open('assets/Skyteam_Timetable.pdf', 'rb')
-    output_file = open('assets/Skyteam_Timetable.txt', 'w+')
+    input_file = open('../assets/Skyteam_Timetable.pdf', 'rb')
+    output_file = open('../assets/Skyteam_Timetable.csv', 'wb')
+    output_file_writer = csv.writer(output_file, delimiter=cell_delimiter)
     print 'Files ready'
 
     PDF_report = PyPDF2.PdfFileReader(input_file)
     print 'PDF opened'
+
+    output_file_writer.writerow(header)
+    print 'Header writen'
 
     print 'Processing'
     for current_page_num in range(start_page_num, end_page_num):
@@ -23,10 +28,12 @@ if __name__ == '__main__':
         matches = re.finditer(regex, page.extractText(), re.MULTILINE)
         for matchNum, match in enumerate(matches):
             matchNum = matchNum + 1
+            row = []
             for groupNum in range(0, len(match.groups())):
                 groupNum = groupNum + 1
                 if groupNum not in nested_regex_group_numbers:
                     groupString = match.group(groupNum)
                     if groupString:
-                        output_file.write(match.group(groupNum) + cell_delimiter)
-            output_file.write(row_delimiter)
+                        row.append(match.group(groupNum))
+            output_file_writer.writerow(row)
+    output_file.close()
