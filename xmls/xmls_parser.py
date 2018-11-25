3451dc6f2f562cdd113c0142d1f9955c9fbe69bd
Kirill Skubnikov <kirilllik@gmail.com>
2018-11-25 11:30:13 +0300

    xmls missing card_number field added

--- xmls/xmls_parser.py
+++ xmls/xmls_parser.py
@@ -9,8 +9,10 @@ if __name__ == '__main__':
     output_file_path = '../assets/parsed/YourBoardingPassDotAero.xmls.csv'
 
     header = ['id', 'honorific', 'name', 'flight_code', 'departure', 'arrival', 'departure_code', 'arrival_code',
-              'departure_date', 'departure_time', 'e_ticket', 'class', 'pnr', 'operated_by', 'seat', 'sequence']
-    cell_coordinates = ['A3', 'B3', 'A5', 'D5', 'H5', 'D7', 'H7', 'A9', 'C9', 'E13', 'H3', 'B13', 'E9', 'H11', 'H1']
+              'departure_date', 'departure_time', 'e_ticket', 'class', 'pnr', 'operated_by', 'seat', 'sequence',
+              'card_number']
+    cell_coordinates = ['A3', 'B3', 'A5', 'D5', 'H5', 'D7', 'H7', 'A9', 'C9', 'E13', 'H3', 'B13', 'E9', 'H11', 'H1',
+                        'F3']
 
     boarding_pass_idx = 0
     file_counter = 1
